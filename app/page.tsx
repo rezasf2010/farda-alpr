93cd4b7e27a9303ef2d085bd0178de753fbfe778
reza_sf <rezasf2010@gmail.com>
2025-10-15 16:03:08 +0330

    main background and tailwind settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="border border-red-500 flex flex-col justify-center h-screen text-center">
+    <div className=" flex flex-col justify-center text-center">
       <h1 className="text-2xl p-4">Hello World!!!</h1>
       <br />
       <br />
